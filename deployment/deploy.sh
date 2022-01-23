89682ca11a091fc223444e625301461b3bf15afb
ferraobox <ferraobox@hotmail.es>
2022-01-23 06:07:01 +0100

    feat: apply preview check to dependabot builds

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_AUTHOR_NAME" == "dependabot[bot]" || "$VERCEL_GIT_COMMIT_AUTHOR_NAME" == "dependabot" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
